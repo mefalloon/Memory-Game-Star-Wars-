7640c140b768fa5c2bfe6eab002ed4a0bb7145b4
elizafalloon-ga <elizafalloon@gmail.com>
2022-03-22 00:33:05 -0700

    logo inseration and completed hover functionality

--- js/app.js
+++ js/app.js
@@ -1,17 +1,28 @@
 //console.log('hey')
 // array of cards
 const allCards = document.querySelectorAll('.cards');  
-//loop through with a for each /attach a listener/ onclick
+
 function flipCard() {
-    this.classList.toogle('flip');
+    this.classList.toggle('flip')
 }
+
+//loop through with a for each /attach a listener/ onclick
 allCards.forEach(card => card.addEventListener('click', flipCard));
 
-//console.log(allCards);
+// the card needs to wait for one card to be clicked
+
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
 
 
 
 
 
+
+
+
+
+// const cards = Array.from(document.querySelectorAll('card-visible'));
 
 
