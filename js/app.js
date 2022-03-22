c2c5c882f4492594a386e2f4bed70f9897af4798
elizafalloon-ga <elizafalloon@gmail.com>
2022-03-22 10:18:10 -0700

    Flipcard function for all cards

--- js/app.js
+++ js/app.js
@@ -3,26 +3,10 @@
 const allCards = document.querySelectorAll('.cards');  
 
 function flipCard() {
-    this.classList.toggle('flip')
+
+    this.classList.add('flip');
+
+allCards.forEach(card => card.addEventListener('click', flipCard));
 }
 
-//loop through with a for each /attach a listener/ onclick
-allCards.forEach(card => card.addEventListener('click', flipCard));
-
-// the card needs to wait for one card to be clicked
-
-
-
-cards.forEach(card => card.addEventListener('click', flipCard));
-
-
-
-
-
-
-
-
-
-// const cards = Array.from(document.querySelectorAll('card-visible'));
-
-
+console.log(allCards)
