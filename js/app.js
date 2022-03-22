90285f0ae21b2b065e03de5027f1b8981ca9f63b
elizafalloon-ga <elizafalloon@gmail.com>
2022-03-21 19:37:15 -0700

    retrying to connect

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 console.log('hey')
-// array of card images
-const allCards = document.querySelectorAll('cards')
+// array of cards
+const allCards = document.querySelectorAll('cards')  
+//loop through with a for each /attach a listener/ onclick
+function flipCard(){
+    this.classList.toogle('flip');
+}
+
+cards.forEach(card =>)
+
+
+
+
 
 const cards = Array.from(document.querySelectorAll('card-visible'));
 
